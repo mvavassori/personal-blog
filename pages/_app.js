6ed0eeaee933fbd6bdafdd6c33d4e6969c074077
Marco Vavassori <marcovavassori.mv@gmail.com>
2022-04-29 11:37:14 +0200

    The Commit message

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/image/favicon.ico" />
+        {/* <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        /> */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        {/* <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        /> */}
+      </Head>
+
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Navbar />
+        <hr />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
