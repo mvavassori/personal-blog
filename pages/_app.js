c29860c7559c9058151a9ef09728238cd8623ce5
Marco Vavassori <marcovavassori.mv@gmail.com>
2022-05-14 14:52:40 +0200

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,27 @@ import "../styles/globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+
       <Head>
         <link rel="shortcut icon" href="/image/favicon.ico" />
       </Head>
