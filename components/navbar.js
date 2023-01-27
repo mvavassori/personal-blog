ce038f1ffc020afb5b3c8b95fad8da8c1c105fa4
Marco Vavassori <marcovavassori.mv@gmail.com>
2023-01-27 13:57:31 +0100

    twitter handle

--- components/navbar.js
+++ components/navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
         </Link>
         <div className="md:flex space-x-6 ">
           <a
-            href="https://twitter.com/mrcvavassori"
+            href="https://twitter.com/marcovvssr"
             target="_blank"
             rel="noopener noreferrer"
             className="font-semibold text-sky-400 hover:text-sky-500"
