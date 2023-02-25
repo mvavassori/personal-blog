c16fae0b391ccf22be7675e876b0c4d1e6da7f9e
Marco Vavassori <marcovavassori.mv@gmail.com>
2023-02-25 12:17:05 +0100

    fix 2 empty blog-posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,10 @@
 import { getAllPostIds, getPostData } from "../../lib/posts";
-// import Layout from '../../components/layout'
 import Head from "next/head";
 import Date from "../../components/date";
-// import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticPaths() {
   //gets markwdown posts ids
-  const paths = getAllPostIds();
+  const paths = getAllPostIds() || [];
   return {
     paths,
     fallback: false, // If fallback is false, then any paths not returned by getStaticPaths will result in a 404 page.
